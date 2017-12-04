9d8bc89a51c3eef996046cf40cb43fad716722e7
Jeremy DeJournett <dejourn2@illinois.edu>
2017-12-04 17:38:41 -0600

    Lab 6 Code

--- src/lab56pkg/src/KaushikPoddarHernandez5.cpp
+++ src/lab56pkg/src/KaushikPoddarHernandez5.cpp
@@ -1,4 +1,4 @@
- #include "lab56pkg/lab56.h"
+#include "lab56pkg/lab56.h"
 #include <algorithm>
 extern ImageConverter* ic_ptr; //global pointer from the lab56.cpp
 
@@ -121,12 +121,8 @@ void ImageConverter::imageCb(const sensor_msgs::ImageConstPtr& msg)
 // You will implement your algorithm for calculating threshold here.
 Mat ImageConverter::thresholdImage(Mat gray_img)
 {
-	Mat bw_img  = gray_img.clone(); // copy input image to a new image
-	/*if (leftclickdone == 0 || rightclickdone == 0) {
-		return bw_img;
-	}*/
 		int   totalpixels;
-
+		Mat bw_img  = gray_img.clone(); // copy input image to a new image
 		totalpixels	  = gray_img.rows*gray_img.cols;			// total number of pixels in image
 
 		int H[256] = {0};
@@ -187,11 +183,7 @@ Mat ImageConverter::thresholdImage(Mat gray_img)
 // Take an black and white image and find the object it it, returns an associated image with different color for each image
 // You will implement your algorithm for rastering here
 Mat ImageConverter::associateObjects(Mat bw_img)
-{	
-	Mat associate_img = Mat::zeros( bw_img.size(), CV_8UC3 ); // function will return this image
-	/*if (leftclickdone == 0 || rightclickdone == 0) {
-		return associate_img;
-	}*/
+{
 	//initiallize the variables you will use
 	int height,width; // number of rows and colums of image
 	int red, green, blue; //used to assign color of each objects
@@ -327,10 +319,16 @@ Mat ImageConverter::associateObjects(Mat bw_img)
 			}
 		}
 	}
-	int num_objects = cur_labels.size();
-	//cout << "Number of Objects: " << num_objects << endl;
+	/*max_val = 0;
+	for (int i=0; i<10000; i++) {
+		if (objectsize[i] > max_val) {
+			max_val = objectsize[i];
+		}
+	}*/
+	//cout << "Max Size: " << max_val << endl;
+	cout << "Number of Objects: " << cur_labels.size() << endl;
 	int old_label;
-	for (int i=0; i<num_objects; i++) {
+	for (int i=0; i<cur_labels.size(); i++) {
 		old_label = cur_labels[i];
 		for (int r=0; r<height; r++) {
 			for (int c=0; c<width; c++) {
@@ -341,37 +339,8 @@ Mat ImageConverter::associateObjects(Mat bw_img)
 		}
 	}
 
-	float m00[num_objects];
-	float m01[num_objects];
-	float m10[num_objects];
-	float rbar[num_objects];
-	float cbar[num_objects];
-	for (int k=0; k<num_objects; k++) {
-		m00[k] = 0.0;
-		m01[k] = 0.0;
-		m10[k] = 0.0;
-		rbar[k] = 0.0;
-		cbar[k] = 0.0;
-	}
-
-	for (int r=0; r<height; r++) {
-		for (int c=0; c<width; c++) {
-			int k = pixellabel[r][c]-1;
-			m10[k] += r;
-			m01[k] += c;
-			m00[k] += 1;
-		}
-	}
-
-
-	for (int k=0; k<num_objects; k++) {
-		rbar[k] = m10[k]/m00[k];
-		cbar[k] = m01[k]/m00[k];
-		//cout << "Object "<< k+1 << "is at " << rbar[k] << " and " << cbar[k] << endl;
-	}
-
 	// assign UNIQUE color to each object
-	
+	Mat associate_img = Mat::zeros( bw_img.size(), CV_8UC3 ); // function will return this image
 	Vec3b color;
 	for(int row=0; row<height; row++)
 	{
@@ -443,36 +412,7 @@ Mat ImageConverter::associateObjects(Mat bw_img)
 			associate_img.at<Vec3b>(Point(col,row)) = color;
 		}
 	}
-	for (int k=0; k<num_objects; k++) {
-		int r_cen = (int) rbar[k]; int c_cen = (int) cbar[k];
-		if (r_cen + 5 < 240*2 && c_cen + 5 < 320*2 && r_cen - 5 > 0 && c_cen - 5 > 0) {
-			color[0] = 255; color[1] = 255; color[2] = 255;
-			//cout << r_cen << " , " << c_cen << endl;
-			for (int i=-5; i<6; i++) {
-				associate_img.at<Vec3b>(Point(c_cen+i, r_cen)) = color;
-				associate_img.at<Vec3b>(Point(c_cen, r_cen+i)) = color;
-			}
-		}
-	}	
-	//out << "hi" << endl;
-	// Calculate Beta
-	/*float d = 10.0/100; //distance in m in world
-	cout << rbar[0] << "," << cbar[0] << "," << rbar[1] << "," << cbar[1] << endl;
-	float pixel_d = sqrt(pow(rbar[1] - rbar[0], 2) + pow(cbar[1]-cbar[0], 2));
-	cout << "pixel_d: " << pixel_d << "; d: " << d << "," << pixel_d/d << endl;
-	float beta = pixel_d/d;
-	cout << "Beta: " << beta << endl;
-
-	//Calculate Theta
-	float theta = atan2(rbar[1]-rbar[0], cbar[1] - cbar[0]);
-	cout << "Theta: " << theta <<endl;
-
-	//Calculate Tx Ty
-	float xw_1 = 20/100;
-	float yw_1 = 0;
-	float T_x = (rbar[1] - 0.5*height)/beta - cos(theta)*xw_1 + sin(theta)*yw_1;
-	float T_y = (cbar[1] - 0.5*width)/beta - sin(theta)*xw_1 - cos(theta)*yw_1;
-	cout << "T_x: " << T_x << " , T_y: " << T_y << " o_r: " << 0.5*height << " o_c: " << 0.5*width << endl;*/
+	
 	return associate_img;
 }
 
@@ -487,19 +427,9 @@ Mat ImageConverter::associateObjects(Mat bw_img)
 void onMouse(int event, int x, int y, int flags, void* userdata)
 {
 		ic_ptr->onClick(event,x,y,flags,userdata);
-
 }
 void ImageConverter::onClick(int event,int x, int y, int flags, void* userdata)
 {
-	float beta = 800.056;
-	float T_x = -0.100297;
-	float T_y = 0.0913649;
-	float theta = -0.00038072;
-	float o_r = 240;
-	float o_c = 320;
-	float z_down = 0.017;
-	float z_up = 0.25;
-
 	// For use with Lab 6
 	// If the robot is holding a block, place it at the designated row and column. 
 	if  ( event == EVENT_LBUTTONDOWN ) //if left click, do nothing other than printing the clicked point
@@ -507,102 +437,11 @@ void ImageConverter::onClick(int event,int x, int y, int flags, void* userdata)
 		if (leftclickdone == 1) {
 			leftclickdone = 0;  // code started
 			ROS_INFO_STREAM("left click:  (" << x << ", " << y << ")");  //the point you clicked
-			if (SuctionValue < 1.5) {
-				float x_c = -(y-o_r)/beta-0.00662261;
-				float y_c = -(x-o_c)/beta+0.17;
-				float x_w = cos(theta)*(x_c - T_x) + sin(theta)*(y_c - T_y);
-				float y_w = -sin(theta)*(x_c - T_x) + cos(theta)*(y_c - T_y);
-				cout << x_w*100 << "," << y_w*100 << endl;
 
-				driver_msg.destination=lab_invk(x_w, y_w, z_up, 45);
+			// put your left click code here
 
-				//publish the point to the robot
-			    ros::Rate loop_rate(SPIN_RATE); // Initialize the rate to publish to ur3/command
-				int spincount = 0;
-				driver_msg.duration = 3.0;
-				pub_command.publish(driver_msg);  // publish command, but note that is possible that
-													  // the subscriber will not receive this message.
-				spincount = 0;
-				while (isReady) { // Waiting for isReady to be false meaning that the driver has the new command
-					ros::spinOnce();  // Allow other ROS functionallity to run
-					loop_rate.sleep(); // Sleep and wake up at 1/20 second (1/SPIN_RATE) interval
-					if (spincount > SPIN_RATE) {  // if isReady does not get set within 1 second re-publish
-						pub_command.publish(driver_msg);
-						ROS_INFO_STREAM("Just Published again driver_msg");
-						spincount = 0;
-					}
-					spincount++;  // keep track of loop count
-				}
-				ROS_INFO_STREAM("waiting for rdy");  // Now wait for robot arm to reach the commanded waypoint.
-				
-				while(!isReady)
-				{
-					ros::spinOnce();
-					loop_rate.sleep();
-				}
-				ROS_INFO_STREAM("Ready for new point");
 
-				driver_msg.destination=lab_invk(x_w, y_w, z_down, 45);
-
-				//publish the point to the robot
-				pub_command.publish(driver_msg);  // publish command, but note that is possible that
-													  // the subscriber will not receive this message.
-				spincount = 0;
-				while (isReady) { // Waiting for isReady to be false meaning that the driver has the new command
-					ros::spinOnce();  // Allow other ROS functionallity to run
-					loop_rate.sleep(); // Sleep and wake up at 1/20 second (1/SPIN_RATE) interval
-					if (spincount > SPIN_RATE) {  // if isReady does not get set within 1 second re-publish
-						pub_command.publish(driver_msg);
-						ROS_INFO_STREAM("Just Published again driver_msg");
-						spincount = 0;
-					}
-					spincount++;  // keep track of loop count
-				}
-				ROS_INFO_STREAM("waiting for rdy");  // Now wait for robot arm to reach the commanded waypoint.
-				
-				while(!isReady)
-				{
-					ros::spinOnce();
-					loop_rate.sleep();
-				}
-				ROS_INFO_STREAM("Ready for new point");
-
-				srv.request.fun = 1;
-				srv.request.pin = 0;  //Digital Output 0
-				srv.request.state = 1.0; //Set DO0 on
-				if (srv_SetIO.call(srv)) {
-					ROS_INFO("True: Switched Suction ON");
-					SuctionValue = 2;
-				} else {
-					ROS_INFO("False");
-				}
-				driver_msg.destination=lab_invk(x_w, y_w, z_up, 45);
-
-				//publish the point to the robot
-				pub_command.publish(driver_msg);  // publish command, but note that is possible that
-													  // the subscriber will not receive this message.
-				spincount = 0;
-				while (isReady) { // Waiting for isReady to be false meaning that the driver has the new command
-					ros::spinOnce();  // Allow other ROS functionallity to run
-					loop_rate.sleep(); // Sleep and wake up at 1/20 second (1/SPIN_RATE) interval
-					if (spincount > SPIN_RATE) {  // if isReady does not get set within 1 second re-publish
-						pub_command.publish(driver_msg);
-						ROS_INFO_STREAM("Just Published again driver_msg");
-						spincount = 0;
-					}
-					spincount++;  // keep track of loop count
-				}
-				ROS_INFO_STREAM("waiting for rdy");  // Now wait for robot arm to reach the commanded waypoint.
-				
-				while(!isReady)
-				{
-					ros::spinOnce();
-					loop_rate.sleep();
-				}
-				ROS_INFO_STREAM("Ready for new point");
 			leftclickdone = 1; // code finished
-		}
-
 		} else {
 			ROS_INFO_STREAM("Previous Left Click not finshed, IGNORING this Click"); 
 		}
@@ -612,102 +451,12 @@ void ImageConverter::onClick(int event,int x, int y, int flags, void* userdata)
 		if (rightclickdone == 1) {  // if previous right click not finished ignore
 			rightclickdone = 0;  // starting code
 			ROS_INFO_STREAM("right click:  (" << x << ", " << y << ")");  //the point you clicked
+
 			// put your right click code here
-				if (SuctionValue > 1.5) {
-				float x_c = -(y-o_r)/beta-0.00662261;
-				float y_c = -(x-o_c)/beta+0.17;
-				float x_w = cos(theta)*(x_c - T_x) + sin(theta)*(y_c - T_y);
-				float y_w = -sin(theta)*(x_c - T_x) + cos(theta)*(y_c - T_y);
-				cout << x_w*100 << "," << y_w*100 << endl;
 
-				driver_msg.destination=lab_invk(x_w, y_w, z_up, 45);
 
-				//publish the point to the robot
-			    ros::Rate loop_rate(SPIN_RATE); // Initialize the rate to publish to ur3/command
-				int spincount = 0;
-				driver_msg.duration = 3.0;
-				pub_command.publish(driver_msg);  // publish command, but note that is possible that
-													  // the subscriber will not receive this message.
-				spincount = 0;
-				while (isReady) { // Waiting for isReady to be false meaning that the driver has the new command
-					ros::spinOnce();  // Allow other ROS functionallity to run
-					loop_rate.sleep(); // Sleep and wake up at 1/20 second (1/SPIN_RATE) interval
-					if (spincount > SPIN_RATE) {  // if isReady does not get set within 1 second re-publish
-						pub_command.publish(driver_msg);
-						ROS_INFO_STREAM("Just Published again driver_msg");
-						spincount = 0;
-					}
-					spincount++;  // keep track of loop count
-				}
-				ROS_INFO_STREAM("waiting for rdy");  // Now wait for robot arm to reach the commanded waypoint.
-				
-				while(!isReady)
-				{
-					ros::spinOnce();
-					loop_rate.sleep();
-				}
-				ROS_INFO_STREAM("Ready for new point");
 
-				driver_msg.destination=lab_invk(x_w, y_w, z_down, 45);
-
-				//publish the point to the robot
-				pub_command.publish(driver_msg);  // publish command, but note that is possible that
-													  // the subscriber will not receive this message.
-				spincount = 0;
-				while (isReady) { // Waiting for isReady to be false meaning that the driver has the new command
-					ros::spinOnce();  // Allow other ROS functionallity to run
-					loop_rate.sleep(); // Sleep and wake up at 1/20 second (1/SPIN_RATE) interval
-					if (spincount > SPIN_RATE) {  // if isReady does not get set within 1 second re-publish
-						pub_command.publish(driver_msg);
-						ROS_INFO_STREAM("Just Published again driver_msg");
-						spincount = 0;
-					}
-					spincount++;  // keep track of loop count
-				}
-				ROS_INFO_STREAM("waiting for rdy");  // Now wait for robot arm to reach the commanded waypoint.
-				
-				while(!isReady)
-				{
-					ros::spinOnce();
-					loop_rate.sleep();
-				}
-				ROS_INFO_STREAM("Ready for new point");
-
-				srv.request.fun = 1;
-				srv.request.pin = 0;  //Digital Output 0
-				srv.request.state = 0.0; //Set DO0 on
-				if (srv_SetIO.call(srv)) {
-					ROS_INFO("True: Switched Suction OFF");
-					SuctionValue = 1;
-				} else {
-					ROS_INFO("False");
-				}
-				driver_msg.destination=lab_invk(x_w, y_w, z_up, 45);
-
-				//publish the point to the robot
-				pub_command.publish(driver_msg);  // publish command, but note that is possible that
-													  // the subscriber will not receive this message.
-				spincount = 0;
-				while (isReady) { // Waiting for isReady to be false meaning that the driver has the new command
-					ros::spinOnce();  // Allow other ROS functionallity to run
-					loop_rate.sleep(); // Sleep and wake up at 1/20 second (1/SPIN_RATE) interval
-					if (spincount > SPIN_RATE) {  // if isReady does not get set within 1 second re-publish
-						pub_command.publish(driver_msg);
-						ROS_INFO_STREAM("Just Published again driver_msg");
-						spincount = 0;
-					}
-					spincount++;  // keep track of loop count
-				}
-				ROS_INFO_STREAM("waiting for rdy");  // Now wait for robot arm to reach the commanded waypoint.
-				
-				while(!isReady)
-				{
-					ros::spinOnce();
-					loop_rate.sleep();
-				}
-				ROS_INFO_STREAM("Ready for new point");
 			rightclickdone = 1; // code finished
-		}
 		} else {
 			ROS_INFO_STREAM("Previous Right Click not finshed, IGNORING this Click"); 
 		}
